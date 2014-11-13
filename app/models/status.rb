9d7c8e3ba60096d95dd81e004cc9dafc1513a361
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-11-13 13:40:18 -0700

    added validations

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,5 @@
 class Status < ActiveRecord::Base
   validates :status, presence: true
   validates :user, presence: true
-  validate :number_of_likes, presence: true
-end
+  validates :number_of_likes, presence: true
+endgit
